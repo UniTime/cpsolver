e1bed49bfb7d83aa8e6c3c1950fccdcf893835d3
Tomáš Müller <muller@unitime.org>
2007-04-16 17:57:15 +0000

    abstract implementation of reservation constraints, following methods are to be implemented by a particular reservation: 	int canEnroll(Student) -- returns either CAN_ENROLL_YES, CAN_ENROLL_NO or CAN_ENROLL_INSTEAD 	boolean canEnrollInstead(Student student, Student insteadOfStudent) -- when CAN_ENROLL_INSTEAD, true if student can be enrolled instead of insteadOfStudent

---
+++ src/net/sf/cpsolver/studentsct/constraint/ReservationOnCourse.java
@@ -0,0 +1,27 @@
+package net.sf.cpsolver.studentsct.constraint;
+
+import net.sf.cpsolver.studentsct.model.Course;
+import net.sf.cpsolver.studentsct.model.Enrollment;
+
+public abstract class ReservationOnCourse extends Reservation {
+    private Course iCourse = null;
+    
+    public ReservationOnCourse(Course course) {
+        super();
+        iCourse = course;
+    }
+    
+    public Course getCourse() {
+        return iCourse;
+    }
+    
+    public boolean isApplicable(Enrollment enrollment) {
+        if (enrollment.getConfig()==null) return false;
+        
+        // Course check -- is it needed?
+        Course courseThisEnrollment = enrollment.getConfig().getOffering().getCourse(enrollment.getStudent());
+        if (courseThisEnrollment==null || !courseThisEnrollment.equals(iCourse)) return false;
+        
+        return true;
+    }
+}
