85c5f3e1532bdde2b7b6ade257ffba9e398cbd31
tomas13 <tomas13@e5151b7e-cabb-5534-7ab8-ba89a8838d1f>
2010-11-10 12:18:35 +0000

    License changed to GNU LGPL version 3 (was version 2.1).

--- trunk/src/net/sf/cpsolver/coursett/heuristics/TimetableComparator.java
+++ trunk/src/net/sf/cpsolver/coursett/heuristics/TimetableComparator.java
@@ -98,11 +98,11 @@ import net.sf.cpsolver.ifs.util.DataProperties;
  * @version CourseTT 1.2 (University Course Timetabling)<br>
  *          Copyright (C) 2006 - 2010 Tomas Muller<br>
  *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
- *          Lazenska 391, 76314 Zlin, Czech Republic<br>
+ *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
  * <br>
  *          This library is free software; you can redistribute it and/or modify
  *          it under the terms of the GNU Lesser General Public License as
- *          published by the Free Software Foundation; either version 2.1 of the
+ *          published by the Free Software Foundation; either version 3 of the
  *          License, or (at your option) any later version. <br>
  * <br>
  *          This library is distributed in the hope that it will be useful, but
@@ -111,9 +111,7 @@ import net.sf.cpsolver.ifs.util.DataProperties;
  *          Lesser General Public License for more details. <br>
  * <br>
  *          You should have received a copy of the GNU Lesser General Public
- *          License along with this library; if not, write to the Free Software
- *          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- *          02110-1301 USA
+ *          License along with this library; if not see <http://www.gnu.org/licenses/>.
  */
 
 public class TimetableComparator implements SolutionComparator<Lecture, Placement> {
--- trunk/src/net/sf/cpsolver/ifs/example/rpp/RPPModel.java
+++ trunk/src/net/sf/cpsolver/ifs/example/rpp/RPPModel.java
@@ -9,7 +9,7 @@ import net.sf.cpsolver.ifs.model.*;
  * 'http://www.fi.muni.cz/~hanka/rpp/'>http://www.fi.muni.cz/~hanka/rpp/</a>)
  * seeks to place a set of randomly generated rectangles (called objects) of
  * different sizes into a larger rectangle (called placement area) in such a way
- * that no objects overlap and all objects’ borders are parallel to the border
+ * that no objects overlap and all objectsï¿½ borders are parallel to the border
  * of the placement area. In addition, a set of allowable placements can be
  * randomly generated for each object. The ratio between the total area of all
  * objects and the size of the placement area will be denoted as the filled area
@@ -17,22 +17,22 @@ import net.sf.cpsolver.ifs.model.*;
  * <br>
  * RPP allows us to generate various instances of the problem similar to a
  * trivial timetabling problem. The correspondence is as follows: the object
- * corresponds to a course to be timetabled – the x-coordinate to its time, the
+ * corresponds to a course to be timetabled ï¿½ the x-coordinate to its time, the
  * y-coordinate to its classroom. For example, a course taking three hours
  * corresponds to an object with dimensions 3x1 (the course should be taught in
  * one classroom only). Each course can be placed only in a classroom of
- * sufficient capacity – we can expect that the classrooms are ordered
+ * sufficient capacity ï¿½ we can expect that the classrooms are ordered
  * increasingly in their size so each object will have a lower bound on its
  * y-coordinate.
  * 
  * @version IFS 1.2 (Iterative Forward Search)<br>
  *          Copyright (C) 2006 - 2010 Tomas Muller<br>
  *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
- *          Lazenska 391, 76314 Zlin, Czech Republic<br>
+ *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
  * <br>
  *          This library is free software; you can redistribute it and/or modify
  *          it under the terms of the GNU Lesser General Public License as
- *          published by the Free Software Foundation; either version 2.1 of the
+ *          published by the Free Software Foundation; either version 3 of the
  *          License, or (at your option) any later version. <br>
  * <br>
  *          This library is distributed in the hope that it will be useful, but
@@ -41,9 +41,7 @@ import net.sf.cpsolver.ifs.model.*;
  *          Lesser General Public License for more details. <br>
  * <br>
  *          You should have received a copy of the GNU Lesser General Public
- *          License along with this library; if not, write to the Free Software
- *          Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- *          02110-1301 USA
+ *          License along with this library; if not see <http://www.gnu.org/licenses/>.
  */
 public class RPPModel extends Model<Rectangle, Location> {
     /** Constructor. */
