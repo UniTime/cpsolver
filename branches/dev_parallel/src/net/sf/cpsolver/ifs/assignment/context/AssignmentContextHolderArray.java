b161d917db29f03ecb516afbcc652d46052f9d78
muller@unitime.org <muller@unitime.org@e5151b7e-cabb-5534-7ab8-ba89a8838d1f>
2014-01-31 13:54:21 +0000

    CPSolver: assignment context  - an assignment context can be used by a class (e.g., a constraint, a extension, a neighborhood selection, or a criterion) to contain any assignment dependent data    - instead of holding the data directly, the class only holds a reference to the appropriate assignment context, which is stored with the assignment and returned from the assignment using the reference  - this package also contains an abstract implementation for a constraint, an extension, a model, and a neighbour selection with an assignment context, so an assignment context can be easily introduced in existing constraints

---
+++ branches/dev_parallel/src/net/sf/cpsolver/ifs/assignment/context/AssignmentContextHolderArray.java
@@ -0,0 +1,54 @@
+package net.sf.cpsolver.ifs.assignment.context;
+
+import java.util.Arrays;
+
+import net.sf.cpsolver.ifs.assignment.Assignment;
+import net.sf.cpsolver.ifs.model.Value;
+import net.sf.cpsolver.ifs.model.Variable;
+
+/**
+ * A simple assignment context holder implementation using array to store all the assignment contexts.
+ * {@link AssignmentContextReference#getIndex()} are used to index the array.
+ * 
+ * @see AssignmentContext
+ * @see AssignmentContextReference
+ * 
+ * @version IFS 1.2 (Iterative Forward Search)<br>
+ *          Copyright (C) 2014 Tomas Muller<br>
+ *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
+ *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
+ * <br>
+ *          This library is free software; you can redistribute it and/or modify
+ *          it under the terms of the GNU Lesser General Public License as
+ *          published by the Free Software Foundation; either version 3 of the
+ *          License, or (at your option) any later version. <br>
+ * <br>
+ *          This library is distributed in the hope that it will be useful, but
+ *          WITHOUT ANY WARRANTY; without even the implied warranty of
+ *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *          Lesser General Public License for more details. <br>
+ * <br>
+ *          You should have received a copy of the GNU Lesser General Public
+ *          License along with this library; if not see <http://www.gnu.org/licenses/>.
+ **/
+public class AssignmentContextHolderArray<V extends Variable<V, T>, T extends Value<V, T>> implements AssignmentContextHolder<V, T> {
+    private AssignmentContext[] iContexts = new AssignmentContext[1000];
+
+    public AssignmentContextHolderArray() {
+    }
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public <U extends AssignmentContext> U getAssignmentContext(Assignment<V, T> assignment, AssignmentContextReference<V, T, U> reference) {
+        try {
+            AssignmentContext context = iContexts[reference.getIndex()];
+            if (context != null) return (U) context;
+        } catch (ArrayIndexOutOfBoundsException e) {}
+
+        U context = reference.getParent().createAssignmentContext(assignment);
+        if (iContexts.length <= reference.getIndex())
+            iContexts = Arrays.copyOf(iContexts, reference.getIndex() + 1000);
+        iContexts[reference.getIndex()] = context;
+        return context;
+    }
+}
