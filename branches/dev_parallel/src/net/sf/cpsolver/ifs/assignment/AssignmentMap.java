7ff72f029dab8e4c8a19273176b359b25cae1779
muller@unitime.org <muller@unitime.org@e5151b7e-cabb-5534-7ab8-ba89a8838d1f>
2014-01-31 13:52:05 +0000

    CPSolver: assignment model  - an assignment holds current values of all the variables of a model  - these classes decouple an assignment of variables to their values from the model  - this is needed for any kind of parallel computations, or in general, to be able to hold multiple different assignments in memory

---
+++ branches/dev_parallel/src/net/sf/cpsolver/ifs/assignment/AssignmentMap.java
@@ -0,0 +1,84 @@
+package net.sf.cpsolver.ifs.assignment;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import net.sf.cpsolver.ifs.assignment.context.AssignmentContextHolder;
+import net.sf.cpsolver.ifs.model.Value;
+import net.sf.cpsolver.ifs.model.Variable;
+
+/**
+ * An assignment using a {@link HashMap} to store values of all the variables of the model.
+ * This class is slower to store and retrieve a value than {@link AssignmentArray}, but 
+ * faster in retrieving a collection of assigned variables and values (methods 
+ * {@link Assignment#assignedVariables()} and {@link Assignment#assignedValues()}). 
+ * 
+ * @see Assignment
+ * 
+ * @version IFS 1.2 (Iterative Forward Search)<br>
+ *          Copyright (C) 2014 Tomas Muller<br>
+ *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
+ *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
+ * <br>
+ *          This library is free software; you can redistribute it and/or modify
+ *          it under the terms of the GNU Lesser General Public License as
+ *          published by the Free Software Foundation; either version 3 of the
+ *          License, or (at your option) any later version. <br>
+ * <br>
+ *          This library is distributed in the hope that it will be useful, but
+ *          WITHOUT ANY WARRANTY; without even the implied warranty of
+ *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *          Lesser General Public License for more details. <br>
+ * <br>
+ *          You should have received a copy of the GNU Lesser General Public
+ *          License along with this library; if not see <http://www.gnu.org/licenses/>.
+ **/
+public class AssignmentMap<V extends Variable<V, T>, T extends Value<V, T>> extends AssignmentAbstract<V, T> {
+    private Map<V, T> iAssignments = new LinkedHashMap<V, T>();
+    private Map<V, Long> iIteration = new HashMap<V, Long>();
+    
+    /** Creates an empty assignment */
+    public AssignmentMap(AssignmentContextHolder<V, T> contexts) {
+        super(contexts);
+    }
+    
+    @Override
+    public long getIteration(V variable) {
+        Long it = iIteration.get(variable);
+        return (it == null ? 0 : it);
+    }
+
+    @Override
+    public Collection<V> assignedVariables() {
+        return iAssignments.keySet();
+    }
+    
+    @Override
+    public Collection<T> assignedValues() {
+        return iAssignments.values();
+    }
+
+    @Override
+    public int nrAssignedVariables() {
+        return iAssignments.size();
+    }
+    
+    @Override
+    protected T getValueInternal(V variable) {
+        return iAssignments.get(variable);
+   }
+
+    @Override
+    protected void setValueInternal(long iteration, V variable, T value) {
+        if (value == null) {
+            iAssignments.remove(variable);
+            iIteration.remove(variable);
+        } else {
+            iAssignments.put(variable, value);
+            if (iteration > 0)
+                iIteration.put(variable, iteration);
+        }
+    }
+}
