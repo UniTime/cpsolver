UniTime/cpsolver

commit 041a744eba5b8d5673a52c6c0c3b5ed6b93cfcc6

Course Timetabling: Workday Student Conflicts
- added work-day student conflicts
  - there is a work-day conflict when the time between the start of the first class and the end of the last class on a day is too long (e.g., more than 8 hours)
  - solver parameter StudentConflict.WorkDayLimit defines the number of slots of a work-day (default is -1, work-day conflicts are disabled)

src/org/cpsolver/coursett/criteria/StudentConflict.java
@@ -65,7 +65,7 @@ public class StudentConflict extends TimetablingCriterion {
     }
 
     public static boolean overlaps(Placement p1, Placement p2) {
-        return p1 != null && p2 != null && p1.getTimeLocation().hasIntersection(p2.getTimeLocation()) && (!p1.variable().isCommitted() || !p2.variable().isCommitted());
+        return p1 != null && p2 != null && p1.getTimeLocation().hasIntersection(p2.getTimeLocation()) && (!p1.variable().isCommitted() || !p2.variable().isCommitted()) && slots(p1, p2) <= 72;
     }
     
     protected double jointEnrollment(JenrlConstraint jenrl, Placement p1, Placement p2) {
@@ -97,6 +97,18 @@ public class StudentConflict extends TimetablingCriterion {
             }
         }
         return false;
+    }
+    
+    public static int slots(Placement p1, Placement p2) {
+        if (p1 == null || p2 == null) return 0;
+        TimeLocation t1 = p1.getTimeLocation(), t2 = p2.getTimeLocation();
+        if (t1 == null || t2 == null || !t1.shareDays(t2) || !t1.shareWeeks(t2)) return 0;
+        return Math.max(t1.getStartSlot() + t1.getLength(), t2.getStartSlot() + t2.getLength()) - Math.min(t1.getStartSlot(), t2.getStartSlot());
+    }
+    
+    public static boolean workday(int slotsLimit, Placement p1, Placement p2) {
+        if (slotsLimit <= 0) return false;
+        return p1 != null && p2 != null && slots(p1, p2) > slotsLimit;
     }
     
     public static boolean ignore(Lecture l1, Lecture l2) {

src/org/cpsolver/coursett/Test.java
@@ -614,7 +614,7 @@ public class Test implements SolutionListener<Lecture, Placement> {
                 } else if (jenrl.first().values(solution.getAssignment()).size() == 1 && jenrl.second().values(solution.getAssignment()).size() == 1) {
                     Placement p1 = jenrl.first().values(solution.getAssignment()).get(0);
                     Placement p2 = jenrl.second().values(solution.getAssignment()).get(0);
-                    if (JenrlConstraint.isInConflict(p1, p2, ((TimetableModel)p1.variable().getModel()).getDistanceMetric())) {
+                    if (JenrlConstraint.isInConflict(p1, p2, ((TimetableModel)p1.variable().getModel()).getDistanceMetric(), ((TimetableModel)p1.variable().getModel()).getStudentWorkDayLimit())) {
                         nrInevitableStudentConflicts += jenrl.getJenrl();
                         pw.println("Inevitable " + jenrl.getJenrl()
                                 + (p1.getTimeLocation().hasIntersection(p2.getTimeLocation()) ? "" : " distance")

src/org/cpsolver/coursett/constraint/GroupConstraint.java
@@ -396,7 +396,7 @@ public class GroupConstraint extends ConstraintWithContext<Lecture, Placement, G
         SAME_STUDENTS("SAME_STUDENTS", "Same Students", new PairCheck() {
             @Override
             public boolean isSatisfied(GroupConstraint gc, Placement plc1, Placement plc2) {
-                return !JenrlConstraint.isInConflict(plc1, plc2, ((TimetableModel)gc.getModel()).getDistanceMetric());
+                return !JenrlConstraint.isInConflict(plc1, plc2, ((TimetableModel)gc.getModel()).getDistanceMetric(), ((TimetableModel)gc.getModel()).getStudentWorkDayLimit());
             }
             @Override
             public boolean isViolated(GroupConstraint gc, Placement plc1, Placement plc2) {

src/org/cpsolver/coursett/constraint/JenrlConstraint.java
@@ -80,7 +80,7 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
         Lecture other = another(value.variable());
         if (other == null) return;
         Placement otherPlacement = assignment.getValue(other);
-        if (otherPlacement != null && !other.isCommitted() && isInConflict(value, otherPlacement, getDistanceMetric()))
+        if (otherPlacement != null && !other.isCommitted() && isInConflict(value, otherPlacement, getDistanceMetric(), getWorkDayLimit()))
             conflicts.add(otherPlacement);
     }
 
@@ -90,7 +90,7 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
         Lecture other = another(value.variable());
         if (other == null) return false;
         Placement otherPlacement = assignment.getValue(other);
-        return otherPlacement != null && !other.isCommitted() && isInConflict(value, otherPlacement, getDistanceMetric());
+        return otherPlacement != null && !other.isCommitted() && isInConflict(value, otherPlacement, getDistanceMetric(), getWorkDayLimit());
     }
 
     /**
@@ -99,10 +99,11 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
      * @param p1 first placement
      * @param p2 second placement
      * @param m distance metrics
+     * @param workDayLimit limit on the work-day
      * @return true if there is a student conflict between the two placements
      */
-    public static boolean isInConflict(Placement p1, Placement p2, DistanceMetric m) {
-        return p1 != null && p2 != null && !StudentConflict.ignore(p1.variable(), p2.variable()) && (StudentConflict.distance(m, p1, p2) || StudentConflict.overlaps(p1, p2));
+    public static boolean isInConflict(Placement p1, Placement p2, DistanceMetric m, int workDayLimit) {
+        return p1 != null && p2 != null && !StudentConflict.ignore(p1.variable(), p2.variable()) && (StudentConflict.distance(m, p1, p2) || StudentConflict.overlaps(p1, p2) || StudentConflict.workday(workDayLimit, p1, p2));
     }
 
     /**
@@ -116,11 +117,15 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
     public long jenrl(Assignment<Lecture, Placement> assignment, Lecture variable, Placement value) {
         Lecture other = (first().equals(variable) ? second() : first());
         Placement otherPlacement = (other == null ? null : assignment.getValue(other));
-        return (otherPlacement != null && isInConflict(value, otherPlacement, getDistanceMetric()) ? Math.round(iJenrl) : 0);
+        return (otherPlacement != null && isInConflict(value, otherPlacement, getDistanceMetric(), getWorkDayLimit()) ? Math.round(iJenrl) : 0);
     }
     
     private DistanceMetric getDistanceMetric() {
         return (getModel() == null ? null : ((TimetableModel)getModel()).getDistanceMetric());
+    }
+    
+    private int getWorkDayLimit() {
+        return (getModel() == null ? null : ((TimetableModel)getModel()).getStudentWorkDayLimit());
     }
 
     /** True if the given two lectures overlap in time 
@@ -243,6 +248,10 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
     public boolean areStudentConflictsDistance(Assignment<Lecture, Placement> assignment, Placement value) {
         return StudentConflict.distance(getDistanceMetric(), value, assignment.getValue(another(value.variable())));
     }
+    
+    public boolean areStudentConflictsWorkday(Assignment<Lecture, Placement> assignment, Placement value) {
+        return StudentConflict.workday(getWorkDayLimit(), value, assignment.getValue(another(value.variable())));
+    }
 
     public boolean isOfTheSameProblem() {
         return ToolBox.equals(first().getSolverGroupId(), second().getSolverGroupId());
@@ -279,7 +288,7 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
         public JenrlConstraintContext(Assignment<Lecture, Placement> assignment) {
             Placement p1 = assignment.getValue(first());
             Placement p2 = assignment.getValue(second());
-            if (p1 != null && p2 != null && isInConflict(p1, p2, getDistanceMetric())) {
+            if (p1 != null && p2 != null && isInConflict(p1, p2, getDistanceMetric(), getWorkDayLimit())) {
                 iAdded = true;
                 first().addActiveJenrl(assignment, JenrlConstraint.this);
                 second().addActiveJenrl(assignment, JenrlConstraint.this);
@@ -293,7 +302,7 @@ public class JenrlConstraint extends BinaryConstraintWithContext<Lecture, Placem
             Lecture other = another(value.variable());
             if (other == null) return;
             Placement otherValue = assignment.getValue(other);
-            if (!iAdded && otherValue != null && isInConflict(value, otherValue, getDistanceMetric())) {
+            if (!iAdded && otherValue != null && isInConflict(value, otherValue, getDistanceMetric(), getWorkDayLimit())) {
                 iAdded = true;
                 first().addActiveJenrl(assignment, JenrlConstraint.this);
                 second().addActiveJenrl(assignment, JenrlConstraint.this);

src/org/cpsolver/coursett/criteria/StudentWorkdayConflict.java
@@ -1,0 +1,80 @@
+package org.cpsolver.coursett.criteria;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.cpsolver.coursett.constraint.JenrlConstraint;
+import org.cpsolver.coursett.model.Lecture;
+import org.cpsolver.coursett.model.Placement;
+import org.cpsolver.coursett.model.TimetableModel;
+import org.cpsolver.ifs.assignment.Assignment;
+import org.cpsolver.ifs.util.DataProperties;
+
+/**
+ * Student work-day conflicts. This criterion counts student work-day conflicts between classes.
+ * A work-day conflict occurs when two classes that are attended by the same student (or students)
+ * are placed on the same day (or days) in times that are too far a part. The combinations of classes
+ * that share students are maintained by {@link JenrlConstraint}.
+ * <br>
+ * 
+ * @version CourseTT 1.3 (University Course Timetabling)<br>
+ *          Copyright (C) 2006 - 2014 Tomas Muller<br>
+ *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
+ *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
+ * <br>
+ *          This library is free software; you can redistribute it and/or modify
+ *          it under the terms of the GNU Lesser General Public License as
+ *          published by the Free Software Foundation; either version 3 of the
+ *          License, or (at your option) any later version. <br>
+ * <br>
+ *          This library is distributed in the hope that it will be useful, but
+ *          WITHOUT ANY WARRANTY; without even the implied warranty of
+ *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *          Lesser General Public License for more details. <br>
+ * <br>
+ *          You should have received a copy of the GNU Lesser General Public
+ *          License along with this library; if not see
+ *          <a href='http://www.gnu.org/licenses/'>http://www.gnu.org/licenses/</a>.
+ */
+public class StudentWorkdayConflict extends StudentConflict {
+    
+    public int getStudentWorkDayLimit() {
+        return (getModel() == null ? -1 : ((TimetableModel)getModel()).getStudentWorkDayLimit());
+    }
+
+    @Override
+    public boolean inConflict(Placement p1, Placement p2) {
+        return workday(getStudentWorkDayLimit(), p1, p2);
+    }
+    
+    @Override
+    public boolean isApplicable(Lecture l1, Lecture l2) {
+        return l1 != null && l2 != null && !ignore(l1, l2) && applicable(l1, l2); // all student conflicts (including committed)
+    }
+    
+    @Override
+    public double getWeightDefault(DataProperties config) {
+        return config.getPropertyDouble("Comparator.WorkDayStudentConflictWeight", 0.2);
+    }
+    
+    @Override
+    public String getPlacementSelectionWeightName() {
+        return "Placement.NrWorkDayStudConfsWeight";
+    }
+    
+    @Override
+    public void getInfo(Assignment<Lecture, Placement> assignment, Map<String, String> info) {
+        super.getInfo(assignment, info);
+        double conf = getValue(assignment);
+        if (conf > 0.0)
+            info.put("Workday student conflicts", sDoubleFormat.format(conf));
+    }
+    
+    @Override
+    public void getInfo(Assignment<Lecture, Placement> assignment, Map<String, String> info, Collection<Lecture> variables) {
+        super.getInfo(assignment, info, variables);
+        double conf = getValue(assignment, variables);
+        if (conf > 0.0)
+            info.put("Workday student conflicts", sDoubleFormat.format(conf));
+    }    
+}

src/org/cpsolver/coursett/model/FinalSectioning.java
@@ -650,7 +650,7 @@ public class FinalSectioning {
                     if (jenrl.isInConflict(assignment))
                         delta += jenrl.getJenrlWeight(firstStudent());
                 } else {
-                    if (JenrlConstraint.isInConflict(assignment.getValue(secondLecture()), assignment.getValue(lecture), iModel.getDistanceMetric()))
+                    if (JenrlConstraint.isInConflict(assignment.getValue(secondLecture()), assignment.getValue(lecture), iModel.getDistanceMetric(), iModel.getStudentWorkDayLimit()))
                         delta += firstStudent().getJenrlWeight(secondLecture(), lecture);
                 }
             }
@@ -663,7 +663,7 @@ public class FinalSectioning {
                         if (jenrl.isInConflict(assignment))
                             delta += jenrl.getJenrlWeight(secondStudent());
                     } else {
-                        if (JenrlConstraint.isInConflict(assignment.getValue(firstLecture()), assignment.getValue(lecture), iModel.getDistanceMetric()))
+                        if (JenrlConstraint.isInConflict(assignment.getValue(firstLecture()), assignment.getValue(lecture), iModel.getDistanceMetric(), iModel.getStudentWorkDayLimit()))
                             delta += secondStudent().getJenrlWeight(firstLecture(), lecture);
                     }
                 }
@@ -770,7 +770,7 @@ public class FinalSectioning {
         for (Lecture x : student.getLectures()) {
             if (assignment.getValue(x) == null)
                 continue;
-            if (JenrlConstraint.isInConflict(assignment.getValue(lecture), assignment.getValue(x), iModel.getDistanceMetric()))
+            if (JenrlConstraint.isInConflict(assignment.getValue(lecture), assignment.getValue(x), iModel.getDistanceMetric(), iModel.getStudentWorkDayLimit()))
                 test++;
         }
         test += student.countConflictPlacements(assignment.getValue(lecture));
@@ -982,7 +982,7 @@ public class FinalSectioning {
                         if (jenrl.isInConflict(assignment))
                             delta += jenrl.getJenrlWeight(firstStudent());
                     } else {
-                        if (JenrlConstraint.isInConflict(assignment.getValue(secondLecture), assignment.getValue(lecture), iModel.getDistanceMetric()))
+                        if (JenrlConstraint.isInConflict(assignment.getValue(secondLecture), assignment.getValue(lecture), iModel.getDistanceMetric(), iModel.getStudentWorkDayLimit()))
                             delta += firstStudent().getJenrlWeight(secondLecture, lecture);
                     }
                 }
@@ -1011,7 +1011,7 @@ public class FinalSectioning {
                             if (jenrl.isInConflict(assignment))
                                 delta += jenrl.getJenrlWeight(secondStudent());
                         } else {
-                            if (JenrlConstraint.isInConflict(assignment.getValue(firstLecture), assignment.getValue(lecture), iModel.getDistanceMetric()))
+                            if (JenrlConstraint.isInConflict(assignment.getValue(firstLecture), assignment.getValue(lecture), iModel.getDistanceMetric(), iModel.getStudentWorkDayLimit()))
                                 delta += secondStudent().getJenrlWeight(firstLecture, lecture);
                         }
                     }

src/org/cpsolver/coursett/model/Lecture.java
@@ -644,6 +644,10 @@ public class Lecture extends VariableWithContext<Lecture, Placement, Lecture.Lec
     private DistanceMetric getDistanceMetric() {
         return ((TimetableModel)getModel()).getDistanceMetric();
     }
+    
+    private int getStudentWorkDayLimit() {
+        return ((TimetableModel)getModel()).getStudentWorkDayLimit();
+    }
 
     /**
      * Number of student conflicts caused by the initial assignment of this
@@ -658,7 +662,7 @@ public class Lecture extends VariableWithContext<Lecture, Placement, Lecture.Lec
         for (JenrlConstraint jenrl : jenrlConstraints()) {
             Lecture another = jenrl.another(this);
             if (another.getInitialAssignment() != null)
-                if (JenrlConstraint.isInConflict(value, another.getInitialAssignment(), getDistanceMetric()))
+                if (JenrlConstraint.isInConflict(value, another.getInitialAssignment(), getDistanceMetric(), getStudentWorkDayLimit()))
                     studentConflictsSum += jenrl.getJenrl();
         }
         return studentConflictsSum;
@@ -677,7 +681,7 @@ public class Lecture extends VariableWithContext<Lecture, Placement, Lecture.Lec
         for (JenrlConstraint jenrl : jenrlConstraints()) {
             Lecture another = jenrl.another(this);
             if (another.getInitialAssignment() != null)
-                if (JenrlConstraint.isInConflict(value, another.getInitialAssignment(), getDistanceMetric()))
+                if (JenrlConstraint.isInConflict(value, another.getInitialAssignment(), getDistanceMetric(), getStudentWorkDayLimit()))
                     ret.put(another, jenrl.getJenrl());
         }
         return ret;
@@ -696,7 +700,7 @@ public class Lecture extends VariableWithContext<Lecture, Placement, Lecture.Lec
         for (JenrlConstraint jenrl : jenrlConstraints()) {
             Lecture another = jenrl.another(this);
             if (another.getInitialAssignment() != null)
-                if (JenrlConstraint.isInConflict(value, another.getInitialAssignment(), getDistanceMetric()))
+                if (JenrlConstraint.isInConflict(value, another.getInitialAssignment(), getDistanceMetric(), getStudentWorkDayLimit()))
                     ret.addAll(sameStudents(another));
         }
         return ret;

src/org/cpsolver/coursett/model/Student.java
@@ -299,7 +299,8 @@ public class Student implements Comparable<Student> {
                     && lecture.getSchedulingSubpartId().equals(commitedLecture.getSchedulingSubpartId()))
                 continue;
             if (lecture.isToIgnoreStudentConflictsWith(commitedLecture)) continue;
-            if (JenrlConstraint.isInConflict(commitedPlacement, placement, ((TimetableModel)placement.variable().getModel()).getDistanceMetric()))
+            if (JenrlConstraint.isInConflict(commitedPlacement, placement, ((TimetableModel)placement.variable().getModel()).getDistanceMetric(),
+                    ((TimetableModel)placement.variable().getModel()).getStudentWorkDayLimit()))
                 ret.add(commitedPlacement);
         }
         return ret;

commit b5f78d1864241af40127861726ca55755581060f

Course Timetabling: Student Conflicts
- StudentConflict.overlaps(Placement p1, Placement p2): removed an accidentally added check of ignoring overlap when the time between the start of the first class and the end of the last class is over 6 hours
- this fixes an issue introduced in commit 041a744 (Workday Student Conflicts)

src/org/cpsolver/coursett/criteria/StudentConflict.java
@@ -65,7 +65,7 @@ public class StudentConflict extends TimetablingCriterion {
     }
 
     public static boolean overlaps(Placement p1, Placement p2) {
-        return p1 != null && p2 != null && p1.getTimeLocation().hasIntersection(p2.getTimeLocation()) && (!p1.variable().isCommitted() || !p2.variable().isCommitted()) && slots(p1, p2) <= 72;
+        return p1 != null && p2 != null && p1.getTimeLocation().hasIntersection(p2.getTimeLocation()) && (!p1.variable().isCommitted() || !p2.variable().isCommitted());
     }
     
     protected double jointEnrollment(JenrlConstraint jenrl, Placement p1, Placement p2) {