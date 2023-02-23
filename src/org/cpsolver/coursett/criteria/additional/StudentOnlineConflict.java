e18914089d09bf9ca1b0c91c147ee75db933e7a8
tomas-muller <muller@unitime.org>
2023-02-23 13:36:09 +0100

    Course Timetabling: Student Online Conflicts
    - added an experimental criterion that tries to minimize cases where a student has an online and in-person class on the same day
    - online classes are identified by a regular expression matching the room name and set in the StudentConflict.OnlineRoom parameter
      - it defaults to (?i)ONLINE|
      - classes without a room are considered online when the parameter matches a blank string
      - if a class has multiple rooms, all rooms must be online for the class to be considered online
    - the criterion is weighted by the Comparator.StudentOnlineConflictWeight parameter, defaults to one half of the Comparator.StudentConflictWeight
    - to enable add org.cpsolver.coursett.criteria.additional.StudentOnlineConflict into General.AdditionalCriteria parameter

--- src/org/cpsolver/coursett/criteria/additional/StudentOnlineConflict.java
+++ src/org/cpsolver/coursett/criteria/additional/StudentOnlineConflict.java
@@ -9,21 +9,23 @@ import org.cpsolver.coursett.constraint.JenrlConstraint;
 import org.cpsolver.coursett.criteria.StudentConflict;
 import org.cpsolver.coursett.model.Lecture;
 import org.cpsolver.coursett.model.Placement;
+import org.cpsolver.coursett.model.RoomLocation;
 import org.cpsolver.coursett.model.TimetableModel;
 import org.cpsolver.ifs.assignment.Assignment;
 import org.cpsolver.ifs.util.DataProperties;
 
 /**
- * An experimental criterion that tries to keep student all classes before or after the lunch period.
- * There is a conflict (penalized by Comparator.StudentOverLunchConflictWeight parameter) every time when
- * a student has two classes, one in the morning (starting before or at the lunch period) 
- * and one in the afternoon (starting after lunch period). When StudentConflict.OverLunchSameDayOnly is true,
- * only conflicts between classes that are on the same day are counted. The lunch period is defined by
- * StudentConflict.NoonSlot parameter (defaults to 144).
+ * An experimental criterion that tries to minimize cases where a student has an online and in-person
+ * class on the same day. Online classes are identified by a regular expression matching the room name
+ * and set in the StudentConflict.OnlineRoom parameter (defaults to (?i)ONLINE|). Classes without a 
+ * room are considered online when the StudentConflict.OnlineRoom parameter matches a blank string.
+ * If a class has multiple rooms, all rooms must be online for the class to be considered online. 
+ * The criterion is weighted by the Comparator.StudentOnlineConflictWeight parameter, defaults
+ * to one half of the Comparator.StudentConflictWeight.
  * <br>
  * 
  * @version CourseTT 1.3 (University Course Timetabling)<br>
- *          Copyright (C) 2013 - 2014 Tomas Muller<br>
+ *          Copyright (C) 2013 - 2023 Tomas Muller<br>
  *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
  *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
  * <br>
@@ -41,15 +43,26 @@ import org.cpsolver.ifs.util.DataProperties;
  *          License along with this library; if not see
  *          <a href='http://www.gnu.org/licenses/'>http://www.gnu.org/licenses/</a>.
  */
-public class StudentOverLunchConflict extends StudentConflict {
-    private int iNoonSlot = 144;
-    private boolean iSameDay = true;
+public class StudentOnlineConflict extends StudentConflict {
+    private String iOnlineRoom = null;
     
     @Override
     public void configure(DataProperties properties) {   
         super.configure(properties);
-        iNoonSlot = properties.getPropertyInt("StudentConflict.NoonSlot", 144);
-        iSameDay = properties.getPropertyBoolean("StudentConflict.OverLunchSameDayOnly", true);
+        iOnlineRoom = properties.getProperty("StudentConflict.OnlineRoom", "(?i)ONLINE|");
+    }
+    
+    public boolean isOnline(Placement p) {
+        // no room -- StudentConflict.OnlineRoom must allow for a blank string
+        if (p.getNrRooms() == 0)
+            return "".matches(iOnlineRoom);
+        // one room -- room name must match StudentConflict.OnlineRoom
+        if (p.getNrRooms() == 1)
+            return (p.getRoomLocation().getName() != null && p.getRoomLocation().getName().matches(iOnlineRoom));
+        // multiple rooms -- all rooms must match StudentConflict.OnlineRoom
+        for (RoomLocation r: p.getRoomLocations())
+            if (r.getName() == null || !r.getName().matches(iOnlineRoom)) return false;
+        return true;
     }
     
     /**
@@ -60,20 +73,12 @@ public class StudentOverLunchConflict extends StudentConflict {
     }
     
     /**
-     * Is the given placement in the morning or in the afternoon?
-     */
-    public boolean isMorning(Placement placement) {
-        return placement != null && placement.getTimeLocation().getStartSlot() <= iNoonSlot;
-    }
-    
-    /**
-     * There is a conflict when {@link StudentOverLunchConflict#isMorning(Placement)} differs for the two placements.
-     * When parameter StudentConflict.OverLunchSameDayOnly is true, only conflicts that are on the same day
-     * ({@link StudentOverLunchConflict#shareDays(Placement, Placement)} returns true) are counted.
+     * There is a conflict when {@link StudentOnlineConflict#isOnline(Placement)} differs for the two placements
+     * and they are placed on the same day ({@link StudentOnlineConflict#shareDays(Placement, Placement)} returns true).
      */
     @Override
     public boolean inConflict(Placement p1, Placement p2) {
-        return p1 != null && p2 != null && isMorning(p1) != isMorning(p2) && (!iSameDay || shareDays(p1, p2));
+        return p1 != null && p2 != null && isOnline(p1) != isOnline(p2) && shareDays(p1, p2);
     }
     
     @Override
@@ -83,42 +88,27 @@ public class StudentOverLunchConflict extends StudentConflict {
     
     @Override
     public double getWeightDefault(DataProperties config) {
-        return config.getPropertyDouble("Comparator.StudentOverLunchConflictWeight", 0.1 * config.getPropertyDouble("Comparator.StudentConflictWeight", 1.0));
+        return config.getPropertyDouble("Comparator.StudentOnlineConflictWeight", 0.5 * config.getPropertyDouble("Comparator.StudentConflictWeight", 1.0));
     }
     
     @Override
     public String getPlacementSelectionWeightName() {
-        return "Placement.StudentOverLunchConflictWeight";
+        return "Placement.StudentOnlineConflictWeight";
     }
     
     @Override
     public void getInfo(Assignment<Lecture, Placement> assignment, Map<String, String> info) {
         super.getInfo(assignment, info);
         double conf = getValue(assignment);
-        if (conf > 0.0) {
-            double total = 0;
-            for (JenrlConstraint jenrl: ((TimetableModel)getModel()).getJenrlConstraints()) {
-                if (!jenrl.isToBeIgnored()) {
-                    total += jenrl.jenrl();
-                }
-            }
-            info.put("Student over-lunch conflicts", getPerc(conf, 0.0, total) + "% (" + sDoubleFormat.format(conf) + " / " + sDoubleFormat.format(total) + ", weighted: " + sDoubleFormat.format(getWeightedValue(assignment)) + ")");
-        }
+        if (conf > 0.0)
+            info.put("Student online conflicts", sDoubleFormat.format(conf));
     }
     
     @Override
     public void getInfo(Assignment<Lecture, Placement> assignment, Map<String, String> info, Collection<Lecture> variables) {
         super.getInfo(assignment, info, variables);
         double conf = getValue(assignment, variables);
-        if (conf > 0.0) {
-            Set<JenrlConstraint> jenrls = new HashSet<JenrlConstraint>();
-            double total = 0;
-            for (Lecture lecture: variables) {
-                for (JenrlConstraint jenrl: lecture.jenrlConstraints())
-                    if (jenrls.add(jenrl) && !jenrl.isToBeIgnored())
-                        total += jenrl.jenrl();
-            }
-            info.put("Student over-lunch conflicts", getPerc(conf, 0.0, total) + "% (" + sDoubleFormat.format(conf) + ")");
-        }
+        if (conf > 0.0)
+            info.put("Student online conflicts", sDoubleFormat.format(conf));
     }
 }
