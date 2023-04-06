2f2ae52f21209240892f7e7316f6210a9482a814
Aanandi Pankhania <88778753+Aanandi2802@users.noreply.github.com>
2023-04-06 05:34:41 -0300

    [Refactor: Rename Variables]

--- src/org/cpsolver/exam/reports/ExamInstructorConflicts.java
+++ src/org/cpsolver/exam/reports/ExamInstructorConflicts.java
@@ -18,38 +18,43 @@ import org.cpsolver.ifs.util.CSVFile.CSVField;
  * conflicts into a CSV file. <br>
  * <br>
  * Usage:
- * <pre><code>
- * &nbsp;&nbsp;&nbsp;&nbsp;new ExamInstructorConflicts(model).report().save(file);
- * </code></pre>
- * <br>
  * 
+ * <pre>
+ * <code>
+ * &nbsp;&nbsp;&nbsp;&nbsp;new ExamInstructorConflicts(model).report().save(file);
+ * </code>
+ * </pre>
+ * 
+ * <br>
+ *
  * @version ExamTT 1.3 (Examination Timetabling)<br>
  *          Copyright (C) 2008 - 2014 Tomas Muller<br>
  *          <a href="mailto:muller@unitime.org">muller@unitime.org</a><br>
- *          <a href="http://muller.unitime.org">http://muller.unitime.org</a><br>
- * <br>
+ *          <a href=
+ *          "http://muller.unitime.org">http://muller.unitime.org</a><br>
+ *          <br>
  *          This library is free software; you can redistribute it and/or modify
  *          it under the terms of the GNU Lesser General Public License as
  *          published by the Free Software Foundation; either version 3 of the
  *          License, or (at your option) any later version. <br>
- * <br>
+ *          <br>
  *          This library is distributed in the hope that it will be useful, but
  *          WITHOUT ANY WARRANTY; without even the implied warranty of
  *          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  *          Lesser General Public License for more details. <br>
- * <br>
+ *          <br>
  *          You should have received a copy of the GNU Lesser General Public
- *          License along with this library; if not see
- *          <a href='http://www.gnu.org/licenses/'>http://www.gnu.org/licenses/</a>.
+ *          License along with this library; if not see <a href=
+ *          'http://www.gnu.org/licenses/'>http://www.gnu.org/licenses/</a>.
  */
 public class ExamInstructorConflicts {
     private ExamModel iModel = null;
 
     /**
      * Constructor
-     * 
+     *
      * @param model
-     *            examination timetabling model
+     *                  examination timetabling model
      */
     public ExamInstructorConflicts(ExamModel model) {
         iModel = model;
@@ -57,25 +62,30 @@ public class ExamInstructorConflicts {
 
     /**
      * generate report
-     * @param assignment current assignment
+     * 
+     * @param assignment
+     *                       current assignment
      * @return resultant report
      */
     public CSVFile report(Assignment<Exam, ExamPlacement> assignment) {
         CSVFile csv = new CSVFile();
-        csv.setHeader(new CSVField[] { new CSVField("Instructor"), new CSVField("Type"),
-                new CSVField("Section/Course"), new CSVField("Period"), new CSVField("Day"), new CSVField("Time"),
-                new CSVField("Room"), new CSVField("Distance") });
-        boolean isDayBreakBackToBack = ((InstructorBackToBackConflicts)iModel.getCriterion(InstructorBackToBackConflicts.class)).isDayBreakBackToBack();
-        double backToBackDistance = ((InstructorDistanceBackToBackConflicts)iModel.getCriterion(InstructorDistanceBackToBackConflicts.class)).getBackToBackDistance();
+        csv.setHeader(new CSVField[] { new CSVField("Instructor"), new CSVField("Type"), new CSVField("Section/Course"),
+                new CSVField("Period"), new CSVField("Day"), new CSVField("Time"), new CSVField("Room"),
+                new CSVField("Distance") });
+        boolean isDayBreakBackToBack = ((InstructorBackToBackConflicts) iModel
+                .getCriterion(InstructorBackToBackConflicts.class)).isDayBreakBackToBack();
+        double backToBackDistance = ((InstructorDistanceBackToBackConflicts) iModel
+                .getCriterion(InstructorDistanceBackToBackConflicts.class)).getBackToBackDistance();
         for (ExamInstructor instructor : iModel.getInstructors()) {
             for (ExamPeriod period : iModel.getPeriods()) {
-                int nrExams = instructor.getExams(assignment, period).size();
-                if (nrExams > 1) {
+                int numberOfExams = instructor.getExams(assignment, period).size();
+                if (numberOfExams > 1) {
                     String sections = "";
                     String rooms = "";
                     String periods = String.valueOf(period.getIndex() + 1);
                     String periodDays = period.getDayStr();
                     String periodTimes = period.getTimeStr();
+
                     for (Exam exam : instructor.getExams(assignment, period)) {
                         ExamPlacement placement = assignment.getValue(exam);
                         String roomsThisExam = "";
@@ -107,12 +117,12 @@ public class ExamInstructorConflicts {
                             new CSVField(sections), new CSVField(periods), new CSVField(periodDays),
                             new CSVField(periodTimes), new CSVField(rooms) });
                 }
-                if (nrExams > 0) {
+                if (numberOfExams > 0) {
                     if (period.next() != null && !instructor.getExams(assignment, period.next()).isEmpty()
                             && (!isDayBreakBackToBack || period.next().getDay() == period.getDay())) {
-                        for (Exam ex1 : instructor.getExams(assignment, period)) {
-                            for (Exam ex2 : instructor.getExams(assignment, period.next())) {
-                                ExamPlacement placement = assignment.getValue(ex1);
+                        for (Exam firstExam : instructor.getExams(assignment, period)) {
+                            for (Exam secondExam : instructor.getExams(assignment, period.next())) {
+                                ExamPlacement placement = assignment.getValue(firstExam);
                                 String sections = "";
                                 String rooms = "";
                                 String roomsThisExam = "";
@@ -125,7 +135,7 @@ public class ExamInstructorConflicts {
                                     roomsThisExam += room.getName();
                                 }
                                 boolean first = true;
-                                for (ExamOwner owner : ex1.getOwners(instructor)) {
+                                for (ExamOwner owner : firstExam.getOwners(instructor)) {
                                     if (sections.length() > 0) {
                                         sections += "\n";
                                         rooms += "\n";
@@ -138,11 +148,11 @@ public class ExamInstructorConflicts {
                                         rooms += roomsThisExam;
                                     first = false;
                                 }
-                                if (ex1.getOwners(instructor).isEmpty()) {
-                                    sections += ex1.getName();
+                                if (firstExam.getOwners(instructor).isEmpty()) {
+                                    sections += firstExam.getName();
                                     rooms += roomsThisExam;
                                 }
-                                placement = assignment.getValue(ex2);
+                                placement = assignment.getValue(secondExam);
                                 roomsThisExam = "";
                                 for (ExamRoomPlacement room : placement.getRoomPlacements()) {
                                     if (roomsThisExam.length() > 0)
@@ -150,13 +160,14 @@ public class ExamInstructorConflicts {
                                     roomsThisExam += room.getName();
                                 }
                                 first = true;
-                                for (ExamOwner owner : ex2.getOwners(instructor)) {
+                                for (ExamOwner owner : secondExam.getOwners(instructor)) {
                                     sections += "\n";
                                     rooms += "\n";
                                     periods += "\n";
                                     periodDays += "\n";
                                     periodTimes += "\n";
                                     sections += owner.getName();
+
                                     if (first) {
                                         rooms += roomsThisExam;
                                         periods += String.valueOf(period.next().getIndex() + 1);
@@ -165,28 +176,29 @@ public class ExamInstructorConflicts {
                                     }
                                     first = false;
                                 }
-                                if (ex2.getOwners(instructor).isEmpty()) {
+                                if (secondExam.getOwners(instructor).isEmpty()) {
                                     sections += "\n";
                                     rooms += "\n";
                                     periods += "\n";
                                     periodDays += "\n";
                                     periodTimes += "\n";
-                                    sections += ex2.getName();
+                                    sections += secondExam.getName();
                                     rooms += roomsThisExam;
                                     periods += String.valueOf(period.next().getIndex() + 1);
                                     periodDays += period.next().getDayStr();
                                     periodTimes += period.next().getTimeStr();
                                 }
-                                String distStr = "";
+                                String distanceString = "";
                                 if (backToBackDistance >= 0) {
-                                    double dist = (assignment.getValue(ex1)).getDistanceInMeters(assignment.getValue(ex2));
-                                    if (dist > 0)
-                                        distStr = String.valueOf(dist);
+                                    double gapBetweenTwoExams = (assignment.getValue(firstExam))
+                                            .getDistanceInMeters(assignment.getValue(secondExam));
+                                    if (gapBetweenTwoExams > 0)
+                                        distanceString = String.valueOf(gapBetweenTwoExams);
                                 }
                                 csv.addLine(new CSVField[] { new CSVField(instructor.getName()),
                                         new CSVField("back-to-back"), new CSVField(sections), new CSVField(periods),
                                         new CSVField(periodDays), new CSVField(periodTimes), new CSVField(rooms),
-                                        new CSVField(distStr) });
+                                        new CSVField(distanceString) });
                             }
                         }
                     }
